a43c485634edf7798fd83b336e3ea76bc9224891
Ajchudasama <chudasamaankit100@gmail.com>
2024-01-16 17:28:23 +0530

    New

---
+++ script.js
@@ -0,0 +1,58 @@
+let nav_header = document.querySelector(".header");
+ const navToggler = document.querySelector(".nav-toggler");
+ navToggler.addEventListener("click", navToggle);
+
+ function navToggle() {
+    navToggler.classList.toggle("active");
+    const nav = document.querySelector(".nav");
+    nav.classList.toggle("open");
+    if(nav.classList.contains("open")){
+    	nav.style.maxHeight = nav.scrollHeight + "px";
+    }
+    else{
+    	nav.removeAttribute("style");
+    }
+ } 
+
+ window.addEventListener("scroll", () => {
+   if (window.pageYOffset > 0) {
+       nav_header.classList.add("sticky");
+   } else {
+       nav_header.classList.remove("sticky");
+   }
+});
+
+
+$('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
